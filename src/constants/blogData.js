44cd6ccd9d7f5cb3f7a90c9d8fde18b48e4a84bb
monsterzo <zouhair.toufani-etu@etu.univh2c.ma>
2022-05-20 21:28:25 +0100

    20

--- src/constants/blogData.js
+++ src/constants/blogData.js
@@ -67,7 +67,6 @@ const creatorPara = ({ content }) => {
                         payload: creatorImage({ content: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'})
                     }
                 ]
-
             }
         ]
     }
