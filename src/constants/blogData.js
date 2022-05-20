8c353c0c21e67a4f65eef2eab72ec4d650fb14a1
monsterzo <zouhair.toufani-etu@etu.univh2c.ma>
2022-05-20 21:26:46 +0100

    20

--- src/constants/blogData.js
+++ src/constants/blogData.js
@@ -1,57 +1,73 @@
 const creatorPara = ({ content }) => {
-    return () =>  (
+    return () =>  {
         <div className="creat__para">
             <p>{content}</p>
         </div>
-    )
+        }
 
+    }
+    const creatorTitle=({ content }) =>{
+        return () =>  {
+            <div className="creat__Title">
+                <h3>{content}</h3>
+            </div>
+            }
     }
     const creatorImage = ({ alt, url }) => {
         if (alt.length != 0 && url.length != 0)
             return null;
-        return (
-            alt, url
-        )
-    }
+        return  () => {
+            <div>
+                <img alt={alt} url={url} />
+            </div>       
+         }
+        }
     const blogData = {
         title: 'blog',
         payload: [
             {
-                title: 'Programation',
+                title: creatorTitle({ content : 'Programmation'}),
                 date: '',
                 user: {},
                 payload: [
                     {
                         type: 'paragraphe',
-                        payload: {
-                            url: {},
-                            alt: ''
-                        }
+                        payload: creatorPara({content:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero '})
                     },
                     {
                         type: 'image',
-                        payload: {
-                            url: '',
-                            alt: ""
-                        }
+                        payload: creatorImage({ content: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'})
                     }
                 ]
             }, {
-                title: 'Robotique',
+                title: creatorTitle({content:"Robotique"}),
                 date: '',
                 user: {},
                 payload: [
                     {
-                        type: 'image',
-                        payload: {
-
-
-                        },
-                {
                         type: 'paragraphe',
-                        payload: creatorPara({ content: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:    " })
+                        payload: creatorPara({content:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero '})
+                    },
+                    {
+                        type: 'image',
+                        payload: creatorImage({ content: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'})
                     }
                 ]
+            },{
+                title: creatorTitle({content:"Devoloppement"}),
+                date: '',
+                user: {},
+                payload: [
+                    {
+                        type: 'paragraphe',
+                        payload: creatorPara({content:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero '})
+                    },
+                    {
+                        type: 'image',
+                        payload: creatorImage({ content: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero'})
+                    }
+                ]
+
             }
         ]
     }
