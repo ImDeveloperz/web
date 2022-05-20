cebe71248781406677c58755c4e9669a4eefb007
monsterzo <zouhair.toufani-etu@etu.univh2c.ma>
2022-05-20 16:49:47 +0100

    20

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -12,16 +12,16 @@ function Footer() {
                 <p className="p__opensans">Km 8 Route d'El Jadida, B.P 5366 Maarif Casablanca 20100 Maroc</p>
                 <p className="p__opensans">+212 677-----</p>
                 <p className="p__opensans">+212 566-----</p>
+                <p>  Gmail : Innovinfofsac@gmail.com</p>
            </div>
             <div className="app__footer-links_logo">
                   <img src={images.logo2} alt="app-logo"></img>
                   <p className="p__mottivation">~~Un pour tout et tous pour un~~</p>
             </div>
             <div className="app_footer-links_work">
+              <h3>Suivi Le Club</h3>
                    <p>Instagram : innovinfofsac</p>
                    <p>Facebook : Innovation&Informatique Fsac</p>
-                   <p>  Gmail : Innovinfofsac@gmail.com</p>
-
             </div>
             </div>
             <div className="footer__copyright">
